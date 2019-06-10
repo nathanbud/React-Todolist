fa77f2466b25f8a8af837ef2aaa6ad2eaf43ea83
nathanbud <nthnlbdky916@gmail.com>
2019-06-11 01:07:38 +0800

    Midified App.js and created Todolist.js

---
+++ src/components/TodoItem.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TodoItem = () =>{
+    return(
+        <div>
+       <p> <input type="checkbox"/> List 1</p>
+       <p> <input type="checkbox"/> List 2 </p>
+       <p> <input type="checkbox"/> List 3 </p>
+       <p> <input type="checkbox"/> List 4 </p>
+        </div>
+    )
+}  
+
+export default TodoItem
